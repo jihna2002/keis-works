d3c0814dc11747902eaa1774f12169762102f79b
jihna2002 <jihna2002@yahoo.co.jp>
2019-12-27 13:38:34 +0900

    Heroku Procfile

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+
+@section('title', 'kei\'s works.')
+
+@section('content')
+<header class="header">
+    <p class="site-title-sub">a portofolio of an engineer</p>
+    <h1 class="site-title">Kei's works.</h1>
+</header>
+<section class="works">
+    <h2 class="heading">WORKS</h2>
+</section>
+@endsection
